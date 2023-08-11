b7963a92ea3b78c922f029627396ba2b5ea6fe7b
DD Engineer <48682076+DataDrivenEngineer@users.noreply.github.com>
2023-08-11 14:09:26 -0700

    Fix indentation

--- antiqua/main.m
+++ antiqua/main.m
@@ -31,61 +31,61 @@ void processEvent(NSEvent *e)
 }
 
 int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-	    // Setup code that might create autoreleased objects goes here.
-	    [NSApplication sharedApplication];
-	    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-	    id appDelegate = [AppDelegate alloc];
-	    [NSApp setDelegate:appDelegate];
+  @autoreleasepool {
+    // Setup code that might create autoreleased objects goes here.
+    [NSApplication sharedApplication];
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    id appDelegate = [AppDelegate alloc];
+    [NSApp setDelegate:appDelegate];
 
-	    id menuBar = [NSMenu new];
-	    id appMenuItem = [NSMenuItem new];
-	    [menuBar addItem:appMenuItem];
-	    [NSApp setMainMenu:menuBar];
-	    id appMenu = [NSMenu new];
-	    id appName = [[NSProcessInfo processInfo] processName];
-	    id quitTitle = @"Quit";
-	    id quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle
-	    action:@selector(terminate:) keyEquivalent:@"q"];
-	    [appMenu addItem:quitMenuItem];
-	    [appMenuItem setSubmenu:appMenu];
+    id menuBar = [NSMenu new];
+    id appMenuItem = [NSMenuItem new];
+    [menuBar addItem:appMenuItem];
+    [NSApp setMainMenu:menuBar];
+    id appMenu = [NSMenu new];
+    id appName = [[NSProcessInfo processInfo] processName];
+    id quitTitle = @"Quit";
+    id quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitTitle
+    action:@selector(terminate:) keyEquivalent:@"q"];
+    [appMenu addItem:quitMenuItem];
+    [appMenuItem setSubmenu:appMenu];
 
-	    NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 1000, 600)
-	    styleMask:NSTitledWindowMask | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable
-	    backing:NSBackingStoreBuffered defer:NO];
-	    // Center the window
-	    CGFloat xPos = NSWidth([[window screen] frame]) / 2 - NSWidth([window frame]) / 2;
-	    CGFloat yPos = NSHeight([[window screen] frame]) / 2 + NSHeight([window frame]) / 2;
-	    [window cascadeTopLeftFromPoint:NSMakePoint(xPos,yPos)];
-	    [window setTitle:@"Antiqua"];
-	    [window makeKeyAndOrderFront:nil];
+    NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 1000, 600)
+    styleMask:NSTitledWindowMask | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable
+    backing:NSBackingStoreBuffered defer:NO];
+    // Center the window
+    CGFloat xPos = NSWidth([[window screen] frame]) / 2 - NSWidth([window frame]) / 2;
+    CGFloat yPos = NSHeight([[window screen] frame]) / 2 + NSHeight([window frame]) / 2;
+    [window cascadeTopLeftFromPoint:NSMakePoint(xPos,yPos)];
+    [window setTitle:@"Antiqua"];
+    [window makeKeyAndOrderFront:nil];
 
-	    CustomNSView *view = [[CustomNSView alloc]initWithFrame:[window frame]];
-	    window.contentView = view;
-	    
-	    CustomWindowDelegate *delegate = [[CustomWindowDelegate alloc] init];
-	    window.delegate = delegate;
+    CustomNSView *view = [[CustomNSView alloc]initWithFrame:[window frame]];
+    window.contentView = view;
+    
+    CustomWindowDelegate *delegate = [[CustomWindowDelegate alloc] init];
+    window.delegate = delegate;
 
-	    [NSApp activateIgnoringOtherApps:YES];
-	    [NSApp finishLaunching];
+    [NSApp activateIgnoringOtherApps:YES];
+    [NSApp finishLaunching];
 
-	    while (shouldKeepRunning)
-	    {
-	      @autoreleasepool
-	      {
-		NSEvent *event = [NSApp
-		  nextEventMatchingMask:NSAnyEventMask
-		  untilDate:[NSDate distantPast]
-		  inMode:NSDefaultRunLoopMode
-		  dequeue:YES];
+    while (shouldKeepRunning)
+    {
+      @autoreleasepool
+      {
+	NSEvent *event = [NSApp
+	  nextEventMatchingMask:NSAnyEventMask
+	  untilDate:[NSDate distantPast]
+	  inMode:NSDefaultRunLoopMode
+	  dequeue:YES];
 
-		processEvent(event);
+	processEvent(event);
 
-		[NSApp sendEvent:event];
-		[NSApp updateWindows];
-	      }
-	    }
-	    [NSApp terminate:NSApp];
-	}
-	return 0;
+	[NSApp sendEvent:event];
+	[NSApp updateWindows];
+      }
+    }
+    [NSApp terminate:NSApp];
+  }
+  return 0;
 }
